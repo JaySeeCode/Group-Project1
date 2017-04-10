a9db0f78ad97aae71a9ca3b8ac2af0c9829d1a5c
Juan Carlos Gonzalez Molina <Lametsu@Juans-Air.T-mobile.com>
2017-04-10 13:56:24 -0400

    started working on logic for favorite words site. work in progress

---
+++ assets/javascript/favorite-logic.js
@@ -0,0 +1,48 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBHzsuJjRTjI63IifVncdfhyav-GYuzVR0",
+	authDomain: "team-gosling.firebaseapp.com",
+	databaseURL: "https://team-gosling.firebaseio.com",
+	projectId: "team-gosling",
+	storageBucket: "team-gosling.appspot.com",
+	messagingSenderId: "440069700680"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+database.ref().on("child_added", function(childSnapshot){
+
+	var sv = childSnapshot.val();
+	var key = sv.favoriteWordEntry;
+
+	console.log(sv);
+	console.log(sv.favoriteWordEntry);
+
+	var $button = $('<button />');
+	$button.addClass("removeButton");
+	$button.attr("key", key);
+	$button.text(" X ");
+
+	var $entry = '<li>' + key + '</li>';
+
+
+	// $('#list').prepend(sv.favoriteWordEntry, $button);
+	$('#list').prepend($entry, $button);
+
+	$('.removeButton').on("click", function(){
+
+		console.log($(this).attr("key"));
+
+		var wordToRemove = $(this).attr("key");
+
+		var entryRef = database.ref('favoriteWordEntry');
+
+
+		// database.child(wordToRemove).removeValue();
+
+	});
+
+
+
+});
